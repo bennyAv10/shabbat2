9728c206ded7d6375fae197e4453b06aecad1bea
beniA <b.avramson@gmail.com>
2017-10-20 01:57:54 -0700

    fix

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ const handlers = {
                 }
                     console.log(parashaDate);		
                     
-                    return item.title;
+                    
                 }
             }
         
