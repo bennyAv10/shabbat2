30b37b63a25ca6d37b2c158a4ab5cea6e71f17d2
beniA <b.avramson@gmail.com>
2017-10-20 00:56:42 -0700

    parasha

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const request = require('request');
 
 const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
@@ -121,6 +122,40 @@ const handlers = {
 
         const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
         this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+    },
+    'GetParasha': function() {
+        request('http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=now&ss=on&mf=on&c=on&geo=city&city=CA-Vancouver&m=50&s=on', { json : true}, (err, res, body) => {
+            if (err) {return console.log(err); }
+            //console.log(body);
+            //console.log(body.url);
+            //console.log(body.explanasion);
+            var items = body.items;
+            var selectedParasha="";
+            var selectedDate=futureDate;
+            var today=new Date();
+        
+            for (var item of items) {
+                
+                if (item.category == 'parashat') {
+                    console.log("p:", item.title, "d:", item.date);
+                    var parasha=item.title;
+                    var parashaDate=new Date(item.date);
+                    
+                if (parashaDate>today && parashaDate<selectedDate) {
+                    selectedParasha=parasha;
+                selectedDate=parashaDate;    
+                }
+                    console.log(parashaDate);		
+                    
+                    return item.title;
+                }
+            }
+        
+            console.log("Selected Parasha:", selectedParasha, "SelectedDate:", selectedDate);
+        
+            this.emit(':tell', selectedParasha);
+        })
+        
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
