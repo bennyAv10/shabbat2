f24f693743cb5b19ca5ebf346acdb0a641cbd0f4
beniA <b.avramson@gmail.com>
2017-10-22 07:45:57 -0700

    dependencies and shabbat time

--- logic.js
+++ logic.js
@@ -36,6 +36,58 @@ function getParasha(cb) {
     });
 }
 
+
+function getCalLuach(title, emitHandler) {
+    request('http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=now&ss=on&mf=on&c=on&geo=city&city=CA-Vancouver&m=50&s=on', { json : true}, (err, res, body) => {
+        if (err) {return console.log(err); }
+        //console.log(body);
+        //console.log(body.url);
+        //console.log(body.explanasion);
+        var items = body.items;
+        var selectedTitle="";
+        var selectedDate=futureDate;
+        var today=new Date();
+    
+        for (var item of items) {
+            
+            if (item.category == title) {
+                console.log("p:", item.title, "d:", item.date);
+                var parasha=item.title;
+                var parashaDate=new Date(item.date);
+                console.log("pDate: ", parashaDate);
+            if (parashaDate>today && parashaDate<selectedDate) {
+                selectedTitle=parasha;
+                selectedDate=parashaDate;    
+            }
+                console.log(parashaDate);                
+            }
+        }
+    
+        console.log("Category:", title, "Selected:", selectedTitle, "SelectedDate:", selectedDate);
+    
+        emitHandler(':tell', selectedTitle);
+    });
+}
+
+function getShabbatTime(emitCb) {
+    getCalLuach('candles', emitCb);
+}
+
+function testerCb(cmd, title) {
+    console.log("TesterCB called with: cmd=", cmd, ", title=", title);
+}
+
+function tester() {
+    console.log("hi", process.argv, process.argv[2]);
+    eval(process.argv[2]+"(testerCb)");
+}
+
 module.exports = {
-    getParasha: getParasha
+    getParasha: getParasha,
+    getShabbatTime: getShabbatTime,
+    tester: tester
+};
+
+if (require.main == module) {
+    tester();
 }
