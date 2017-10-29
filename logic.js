27f578548c27bbdcf96f666503f5527c0a045c66
beniA <b.avramson@gmail.com>
2017-10-28 21:40:32 -0700

    #3 Refactor GetParasha

--- logic.js
+++ logic.js
@@ -2,41 +2,8 @@ const request = require('request');
 
 const futureDate = new Date(new Date().getFullYear()+5,0,0,0,0,0,0);
 
-function getParasha(cb) {
-    request('http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=now&ss=on&mf=on&c=on&geo=city&city=CA-Vancouver&m=50&s=on', { json : true}, (err, res, body) => {
-        if (err) {return console.log(err); }
-        //console.log(body);
-        //console.log(body.url);
-        //console.log(body.explanasion);
-        var items = body.items;
-        var selectedParasha="";
-        var selectedDate=futureDate;
-        var today=new Date();
-    
-        for (var item of items) {
-            
-            if (item.category == 'parashat') {
-                console.log("p:", item.title, "d:", item.date);
-                var parasha=item.title;
-                var parashaDate=new Date(item.date);
-                
-            if (parashaDate>today && parashaDate<selectedDate) {
-                selectedParasha=parasha;
-                selectedDate=parashaDate;    
-            }
-                console.log(parashaDate);		
-                
-                
-            }
-        }
-    
-        console.log("Selected Parasha:", selectedParasha, "SelectedDate:", selectedDate);
-    
-        var location = body.location.title;
-        var response = "In " + location + "." + selectedParasha;
-
-        cb(':tell', response);
-    });
+function getParasha(cb, city='') {
+    getCalLuach('parashat', cb, city);
 }
 
 
