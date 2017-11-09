b05f6bd978e08c66c85651f6eb504e422f169ad9
beniA <b.avramson@gmail.com>
2017-11-09 14:59:54 -0800

    #7 - pass city instead of body.city

--- location.js
+++ location.js
@@ -29,7 +29,7 @@ function getUserLocation(deviceId, consentToken, callBack) {
             }
             console.log("Body: ", body, " city type: ", typeof body.city, " city: ", body.city);
             var city = body.city.charAt(0)+body.city.substring(1).toLowerCase();
-            callBack(body.city);
+            callBack(city);
         }
     });
 
